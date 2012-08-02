1d7558835cb998a87969aa51cc13e9441c9ea7f8
Timothy <yexiaozhou2003@gmail.com>
2012-08-02 13:34:06 +0800

    Update copyright

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
     <!-- please don't remove -->
     <div class="footText">
       <!-- you can edit this -->
-      <p>Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a>  2004-2010 | <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> &amp; <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></p>
+      <p>Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a>  2004-2012 | <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> &amp; <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></p>
       <p>Theme by <a target="_blank" href="http://www.spiga.com.mx">Spiga</a> (Modified by <a target="_blank" href="http://www.xiaozhou.net">Timothy</a>)| Powered by: <a target="_blank" href="http://wordpress.org/">Wordpress</a>  <script language="javascript" src="http://count24.51yes.com/click.aspx?id=242284288&logo=1" charset="gb2312"></script></p>
     <p><?php echo get_num_queries(); ?> queries in <?php timer_stop(3); ?> seconds</p>
       <!-- you can edit this -->
